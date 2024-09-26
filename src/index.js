1f4753be06eb3e3de09a216269b70b9158f48dea
HamSilver <kolpakoff@gmail.com>
2024-09-26 09:55:43 +0200

    feat: add 3d model viewers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./style/index.scss";
 
 import { cardLoader } from "./components/cards";
+import { STLViewerEnable } from "./components/threed";
 
 cardLoader("#cards", "load-more");
+
+STLViewerEnable("stlviewer");
