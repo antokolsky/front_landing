6586bbd0b409ebdc36175bcb60d02960ec4f4346
gazik1 <gazimagomed123@mail.ru>
2024-12-22 13:17:50 +0300

    rating updated

--- src/api/vote.js
+++ src/api/vote.js
@@ -1,5 +1,5 @@
 export const vote = (id,type) =>
-    fetch(`http://antokolsky.ddns.net/api/landing/projects/${id}/vote${type==="bottom"?"?action=unvote":""}`, {
+    fetch(`http://antokolsky.ddns.net/api/landing/projects/${id}/vote/${type==="bottom"?"?action=unvote":""}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
     });
