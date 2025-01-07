80b08e0e0284bf2972a747a0c98096ddc52cd2fa
gazik1 <gazimagomed123@mail.ru>
2025-01-07 15:24:52 +0300

    fix url

--- src/api/respodents.js
+++ src/api/respodents.js
@@ -5,11 +5,7 @@ export const respondents = ({
   country,
   activity_type,
 }) =>
-<<<<<<< HEAD
   fetch(`${import.meta.env.VITE_API}/landing/respondents/`, {
-=======
-  fetch("https://antokolsky.ddns.net/api/landing/respondents/", {
->>>>>>> 5421e0cf138b4fa77faf57175f929543fbf07bf7
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
